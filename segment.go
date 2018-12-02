9ce981eae7b153a6a107d869ca47b540decc9ad8
TSC <gst659@gmail.com>
2018-12-02 11:56:31 -0800

    Adding segmentation.

--- segment.go
+++ segment.go
@@ -2,35 +2,71 @@ package eggtimer
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
+
+// A Segment represents the start and stop time of an activity.
+type Segment struct {
+	// The name of the segment
+	Name string
+	// The start of the activity, relative to the overall workflow start.
+	Start time.Duration
+	// The finish of the activity, relative to teh overall workflow start.
+	Finish time.Duration
+	// Errors, if any
+	Error error
+}
 
 // A SegmentDefinition processes lines and determines if they
 // mark the start or end of a given tagged segment.
 type SegmentDefinition interface {
-	Name() string
+	TypeName() string
 	IsStart(line string) string
-	IsStop(line string) string
+	IsFinish(line string) string
 }
 
+// A Segmenter is 
 type Segmenter struct {
 	segTypes []SegmentDefinition
 }
 
-type TagSegment struct {
-	Tag string
-	Opening time.Duration
-	Closing time.Duration
+// AddDefinitions adds a SegmentDefinition to a Segmenter.
+func (this *Segmenter) AddDefinition(d SegmentDefinition) {
+	this.segTypes = append(this.segTypes, d)
 }
 
-func (this Segmenter) segment(events <-chan Event) (result map[string]TagSegment) {
-	var wg sync.WaitGroup
-	wg.Add(1)
+// Segment collects Events and, using its SegmentDefinitions, compiles
+// a table mapping activity names to a Segment struct.
+func (this Segmenter) Collect(events <-chan Event) (activity map[string]*Segment, err error) {
+	activity = make(map[string]*Segment)
 	for e := range events {
-		for i, d := range this.segTypes {
+		err = e.Error
+		if err != nil {
+			return
+		}
+		for _, d := range this.segTypes {
 			if tag := d.IsStart(e.What); tag != "" {
-				fmt.Println(i)
+				name := fmt.Sprintf("%s:%s", d.TypeName(), tag)
+				if segment, ok := activity[name]; !ok {
+					activity[name] = &Segment{
+						Name: name,
+						Start: e.When,
+					}
+				} else {
+					segment.Start = e.When
+				}
+			} else if tag := d.IsFinish(e.What); tag != "" {
+				name := fmt.Sprintf("%s:%s", d.TypeName(), tag)
+				if segment, ok := activity[name]; !ok {
+					activity[name] = &Segment{
+						Name: name,
+						Finish:  e.When,
+					}
+				} else {
+					segment.Finish = e.When
+				}
+			} else {
+				// ignore events that are not entries or exits
 			}
 		}
 	}
